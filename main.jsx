4fb9b170c9a7c7ce4953ef6941f04273c19b36a6
pazguille <guille87paz@gmail.com>
2022-07-29 22:28:36 -0300

    Update quotes.

--- main.jsx
+++ main.jsx
@@ -1,30 +1,26 @@
 /** @jsx h */
-
-import blog, { ga } from 'https://deno.land/x/blog/blog.tsx';
 import { h } from 'https://esm.sh/preact';
-
+import blog, { ga } from 'https://deno.land/x/blog/blog.tsx';
 import Footer from './components/Footer.jsx';
 
 blog({
-  lang: "es-AR",
-  theme: "dark",
-  dateStyle: "long",
-  author: "Guille Paz",
-  title: "Guille Paz",
-  description: "Algunas cosas que tengo en la cabeza y trato de bajar a pantalla.",
-  favicon: "/assets/favicon.ico",
-  avatar: "/assets/avatar.jpg",
-  ogImage: "https://blog.pazguille.me/assets/avatar.jpg",
-  avatarClass: "rounded-full",
+  lang: 'es-AR',
+  theme: 'dark',
+  dateStyle: 'long',
+  author: 'Guille Paz',
+  title: 'Guille Paz',
+  description: 'Algunas cosas que tengo en la cabeza y trato de bajar a pantalla.',
+  favicon: '/assets/favicon.ico',
+  avatar: '/assets/avatar.jpg',
+  ogImage: 'https://blog.pazguille.me/assets/avatar.jpg',
+  avatarClass: 'rounded-full',
   links: [
-    { title: "Email", url: "mailto:guille87paz@gmail.com" },
-    { title: "GitHub", url: "https://github.com/pazguille" },
-    { title: "Twitter", url: "https://twitter.com/pazguille" },
+    { title: 'Email', url: 'mailto:guille87paz@gmail.com' },
+    { title: 'GitHub', url: 'https://github.com/pazguille' },
+    { title: 'Twitter', url: 'https://twitter.com/pazguille' },
   ],
   middlewares: [
     ga('G-7PQP926E3C'),
   ],
   footer: <Footer />,
-  // style: ``,
-  // showHeaderOnPostPage: true,
 });
