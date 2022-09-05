7ee0d1aa28111d3fe4cd3fcc8e501094a8dc9e8f
pazguille <guille87paz@gmail.com>
2022-09-05 08:35:53 -0300

    Fix blog version.

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 /** @jsx h */
 import { h } from 'https://esm.sh/preact';
-import blog, { ga } from 'https://deno.land/x/blog@0.4.2/blog.tsx';
+import blog, { ga } from 'https://deno.land/x/blog/blog.tsx';
 // import blog from 'https://raw.githubusercontent.com/pazguille/deno_blog/main/blog.tsx';
 import Footer from './components/Footer.jsx';
 
